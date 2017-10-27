6d19d619fffef393f6fa741c58b18deb51785a9b
ycgambo <ycgambo@Pro-2.local>
2017-10-27 22:53:05 +0800

    2017/10/27 22:52:59

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 cur_time=$(date '+%Y/%m/%d %H:%M:%S')
-git commit -a -m "$cur_time";
 
-#hexo g;
-#git add -f *; git commit -a -m $cur_time; git push; hexo d;
+hexo g;
+git add -f *; git commit -a -m "$cur_time"; git push; hexo d;
