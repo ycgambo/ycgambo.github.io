72422328ee488a51567858674862513aaf96a560
ycgambo <ycgambo@Pro-2.local>
2017-11-02 17:29:06 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,20 @@
-cur_time=$(date '+%Y/%m/%d %H:%M:%S')
 
-hexo g; hexo d;
-git add -A; git commit -a -m "$cur_time"; git push;
+cur_time=$(date "+%Y/%m/%d %H:%M:%S")
+
+if [ $1 = "test" ]; then
+    hexo g >/dev/null; hexo d >/dev/null; hexo s;
+elif [ $1 = "comment" ]; then
+    if [ -z "$2" ]; then
+        $2 = $cur_time;
+    fi
+    git add -A; git commit -a -m "$2"; git push;
+elif [ $1 = "push" ]; then
+    git add -A; git commit -a -m "$cur_time"; git push;
+elif [ $1 = "COMMENT" ]; then
+    if [ -z "$2" ]; then
+        $2 = $cur_time;
+    fi
+    hexo g >/dev/null; hexo d >/dev/null; git add -A; git commit -a -m "$2"; git push;
+elif [ $1 = "PUSH" ]; then
+    hexo g >/dev/null; hexo d >/dev/null; git add -A; git commit -a -m "$cur_time"; git push;
+fi
